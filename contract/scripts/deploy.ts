6a630347a0d170a0db939c3e19c3abfd3c9c7109
大海 <iwangyang@qq.com>
2025-06-15 19:35:28 +0800

    Update deploy.ts

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -2,14 +2,14 @@ import { ethers } from "hardhat";
 
 async function main() {
   // 获取合约工厂
-  const IPv6Contract = await ethers.getContractFactory("IPv6Contract");
+  const DotContract = await ethers.getContractFactory("DotContract");
 
   // 部署合约并等待部署完成
-  const ipv6Contract = await IPv6Contract.deploy();
+  const dotContract = await DotContract.deploy();
 
   // 获取合约地址
   // @ts-ignore
-  const address = await ipv6Contract.getAddress();
+  const address = await dotContract.getAddress();
 
   // 输出合约地址
   console.log("IPv6Contract deployed to:", address);
