98543566a02be0af900e917c4891c93d26df7f48
大海 <iwangyang@qq.com>
2025-07-23 06:09:21 +0800

    remove PWA

--- app.most.box/next.config.ts
+++ app.most.box/next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-import withPWA from "@ducanh2912/next-pwa";
+// import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -29,13 +29,13 @@ const nextConfig: NextConfig = {
   },
 };
 
-const config = withPWA({
-  dest: "public",
-  register: true,
-  workboxOptions: {
-    skipWaiting: true,
-  },
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+// const config = withPWA({
+//   dest: "public",
+//   register: true,
+//   workboxOptions: {
+//     skipWaiting: true,
+//   },
+//   disable: process.env.NODE_ENV === "development",
+// })(nextConfig);
 
-export default config;
+export default nextConfig;
