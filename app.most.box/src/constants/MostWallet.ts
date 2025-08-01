a59f776b39868d216fbbd276445291e568b17bed
大海 <iwangyang@qq.com>
2025-08-01 16:24:39 +0800

    mostTimestamp

--- app.most.box/src/constants/MostWallet.ts
+++ app.most.box/src/constants/MostWallet.ts
@@ -58,7 +58,18 @@ export const mostEncode = (
   private_key: string
 ) => {
   const bytes = new TextEncoder().encode(text);
-  const nonce = nacl.randomBytes(nacl.box.nonceLength);
+
+  // 生成包含时间戳的 nonce
+  const timestamp = Date.now();
+  const timestampBytes = new Uint8Array(8);
+  const view = new DataView(timestampBytes.buffer);
+  view.setBigUint64(0, BigInt(timestamp));
+
+  const randomBytes = nacl.randomBytes(nacl.box.nonceLength - 8);
+  const nonce = new Uint8Array(nacl.box.nonceLength);
+  nonce.set(timestampBytes, 0);
+  nonce.set(randomBytes, 8);
+
   const encrypted = nacl.box(
     bytes,
     nonce,
@@ -72,19 +83,30 @@ export const mostEncode = (
   return ["mp://2", encodeBase64(nonce), encodeBase64(encrypted)].join(".");
 };
 
+export const mostTimestamp = (data: string): number => {
+  const [prefix, nonce64] = data.split(".");
+  if (prefix !== "mp://2") {
+    console.info("无效的密文");
+    return 0;
+  }
+  const nonce = decodeBase64(nonce64);
+  const view = new DataView(nonce.buffer, nonce.byteOffset, 8);
+  return Number(view.getBigUint64(0));
+};
+
 export const mostDecode = (
   data: string,
   public_key: string,
   private_key: string
 ) => {
-  const [prefix, nonce64, encrypted64] = data.split(".");
+  const [prefix, nonce, encrypted] = data.split(".");
   if (prefix !== "mp://2") {
     console.info("无效的密文");
     return "";
   }
   const decrypted = nacl.box.open(
-    decodeBase64(encrypted64),
-    decodeBase64(nonce64),
+    decodeBase64(encrypted),
+    decodeBase64(nonce),
     new Uint8Array(getBytes(public_key)),
     new Uint8Array(getBytes(private_key))
   );
