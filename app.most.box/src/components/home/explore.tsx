e56999076d7417f65ac25d2f6975255521ac3aa3
大海 <iwangyang@qq.com>
2025-08-19 17:52:13 +0800

    IPFS

--- app.most.box/src/components/home/explore.tsx
+++ app.most.box/src/components/home/explore.tsx
@@ -1,16 +1,41 @@
-// import { useState, useEffect } from "react";
-// import wordsData from "@/assets/json/in-a-word.json";
-import { Text, Container, Title, Accordion, Divider } from "@mantine/core";
+"use client";
+import { useState, useEffect, useRef } from "react";
+import wordsData from "@/assets/json/in-a-word.json";
+import {
+  Text,
+  Container,
+  Title,
+  Accordion,
+  Divider,
+  Group,
+  Box,
+} from "@mantine/core";
+import { useMarkdown } from "@/hooks/useMarkdown";
+import IPFS from "@/assets/docs/IPFS.md";
 import "./explore.scss";
 
 export default function HomeExplore() {
-  // const [randomWord, setRandomWord] = useState("");
+  const [randomWord, setRandomWord] = useState("");
 
-  // useEffect(() => {
-  //   // 随机选择一句话
-  //   const randomIndex = Math.floor(Math.random() * wordsData.length);
-  //   setRandomWord(wordsData[randomIndex]);
-  // }, []);
+  useEffect(() => {
+    // 随机选择一句话
+    const randomIndex = Math.floor(Math.random() * wordsData.length);
+    setRandomWord(wordsData[randomIndex]);
+  }, []);
+
+  const markdown = useMarkdown();
+  const init = async () => {
+    if (ipfsElement.current) {
+      const viewer = await markdown.initViewer(ipfsElement.current);
+      viewer.setMarkdown(IPFS);
+    }
+  };
+
+  const ipfsElement = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    init();
+  }, []);
 
   return (
     <Container py="md">
@@ -18,24 +43,12 @@ export default function HomeExplore() {
       <Text c="dimmed">——「轻松简单、开源免费、部署自己的云盘」</Text>
 
       <Divider my="md" />
-      {/* <Text c="dimmed">{randomWord}</Text> */}
 
       <Accordion my="md" variant="separated" defaultValue="IPFS">
         <Accordion.Item value="IPFS">
           <Accordion.Control icon="🍎">1. IPFS</Accordion.Control>
           <Accordion.Panel>
-            <Text>
-              IPFS (InterPlanetary File System)
-              是一种点对点的分布式文件系统协议。旨在创建一个更开放、更快速、更安全、更持久化的网络，以取代或补充目前主导互联网的
-              http 协议。
-            </Text>
-            <Title size="h4">核心思想：从“位置寻址”到“内容寻址”</Title>
-            <li>
-              IPFS: 你通过一个内容本身的唯一加密哈希值 (称为 CID - Content
-              Identifier)
-              来访问内容。这个哈希值就像是内容的数字指纹，完全由内容本身计算得来。只要内容不变，哈希值就不变。这称为
-              “内容寻址” - 你通过内容本身（其指纹）请求内容。
-            </li>
+            <Box ref={ipfsElement} />
           </Accordion.Panel>
         </Accordion.Item>
 
@@ -90,6 +103,11 @@ export default function HomeExplore() {
           </Accordion.Panel>
         </Accordion.Item>
       </Accordion>
+
+      <Divider my="md" />
+      <Group justify="flex-end">
+        <Text c="dimmed">——「{randomWord}」</Text>
+      </Group>
     </Container>
   );
 }
