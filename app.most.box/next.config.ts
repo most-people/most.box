511f6e23a77ee5bd3a781a3b6eac32df24502b52
大海 <iwangyang@qq.com>
2025-08-19 17:55:56 +0800

    build

--- app.most.box/next.config.ts
+++ app.most.box/next.config.ts
@@ -4,13 +4,31 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   devIndicators: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
   turbopack: {
     rules: {
       "*.svg": {
         loaders: ["@svgr/webpack"],
         as: "*.js",
       },
-      "**/*.md": {
+      "*.md": {
         loaders: ["raw-loader"],
         as: "*.js",
       },
