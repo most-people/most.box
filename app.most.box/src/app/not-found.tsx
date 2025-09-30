e40c636e27701c5e4f1b5f3ff4588fc22bbfac90
SEA <iwangyang@qq.com>
2025-10-01 02:17:39 +0800

    Update not-found.tsx

--- app.most.box/src/app/not-found.tsx
+++ app.most.box/src/app/not-found.tsx
@@ -11,15 +11,13 @@ import PageWebsite from "@/components/not-found/website";
 const Page404 = () => {
   const back = useBack();
   return (
-    <>
+    <Stack gap="md" align="center">
       <Image src="/img/404.svg" alt="404" width={300} height={225} />
-      <Stack gap="md" align="center">
-        <Text c="dimmed">抱歉，你要找的页面不见了</Text>
-        <Button onClick={back} w="100%">
-          返回
-        </Button>
-      </Stack>
-    </>
+      <Text c="dimmed">抱歉，你要找的页面不见了</Text>
+      <Button onClick={back} variant="gradient">
+        返回首页
+      </Button>
+    </Stack>
   );
 };
 
