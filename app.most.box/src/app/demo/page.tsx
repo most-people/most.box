b097d96ece0ac04c3555b34552d958d2e26cb17d
SEA <iwangyang@qq.com>
2025-10-03 10:41:39 +0800

    react

--- app.most.box/src/app/demo/page.tsx
+++ app.most.box/src/app/demo/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { AppHeader } from "@/components/AppHeader";
+import { useUserStore } from "@/stores/userStore";
 import { Container } from "@mantine/core";
+import { useEffect } from "react";
 
 export default function PageDemo() {
+  const wallet = useUserStore((state) => state.wallet);
+
+  useEffect(() => {
+    if (wallet) {
+      console.log(wallet.ed_public_key);
+      console.log(wallet.ed_private_key);
+    }
+  }, [wallet]);
   return (
     <Container py={20}>
       <AppHeader title="demo" />
