18e18f08941183c43c545ee09ce463701460ebd4
大海 <iwangyang@qq.com>
2025-11-26 07:24:21 +0800

    Update ipfs.go

--- dot.most.box/internal/config/ipfs.go
+++ dot.most.box/internal/config/ipfs.go
@@ -1,10 +1,15 @@
 package config
 
 import (
+	"dotmostbox/internal/update"
+
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
 func IPFSAPIBase() string {
+	if update.IsContainer() {
+		return "http://ipfs:5001"
+	}
 	return "http://127.0.0.1:5001"
 }
 
