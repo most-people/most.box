24aeb834c5f6035516996995140a01d3467bd770
大海 <iwangyang@qq.com>
2026-01-26 17:16:08 +0800

    clear package.json

--- app.most.box/next.config.ts
+++ app.most.box/next.config.ts
@@ -4,24 +4,6 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   devIndicators: false,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      type: "asset/source",
-    });
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            icon: true,
-          },
-        },
-      ],
-    });
-    return config;
-  },
   turbopack: {
     rules: {
       "*.svg": {
